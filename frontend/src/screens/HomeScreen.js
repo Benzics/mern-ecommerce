c5a7a92ab87a31c836364df7e8a978e2513350c0
benzics <benjaminnicholas29@gmail.com>
2022-09-09 08:13:24 +0100

    Product component added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import products from '../products'
 import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
 
 const HomeScreen = () => {
   return (
     <>
         <h1>Latest Products</h1>
-        <Row></Row>
+        <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} key={product._id} />
+          </Col>
+        ))}
+        </Row>
     </>
   )
 }

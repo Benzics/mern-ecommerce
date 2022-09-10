600fc439b199452c94888bb07efaedb27c20c658
benzics <benjaminnicholas29@gmail.com>
2022-09-10 21:49:12 +0100

    Single product page now getting data from backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import {
   Row,
@@ -10,10 +10,20 @@ import {
   ListGroupItem,
 } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
+
 const ProductScreen = ({ match }) => {
   const { id } = useParams()
-  const product = products.find((p) => p._id === id)
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const getProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`)
+      setProduct(data)
+    }
+
+    getProduct()
+  }, [])
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
